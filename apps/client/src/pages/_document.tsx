643b4011aabf183dc654448b645e495d8e52e66b
Deglier Amorim <degliersa@gmail.com>
2022-03-10 03:20:14 -0300

    Update Google Fonts

--- apps/client/src/pages/_document.tsx
+++ apps/client/src/pages/_document.tsx
@@ -5,10 +5,13 @@ import { getCssText } from '@ui/web'
 const Document = () => (
   <Html lang="pt-BR">
     <Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link
-        href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;500;600;700&family=Poppins:wght@300;400;500;600;700&display=swap"
         rel="stylesheet"
       />
+
       <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
     </Head>
     <body>
